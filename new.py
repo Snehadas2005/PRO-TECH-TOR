a09df319015c674e23340266ffbc5e73364a7862
Vimal Singh <157964050+its-Vimal@users.noreply.github.com>
2024-09-05 23:12:26 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,34 @@
+import geocoder
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+class LocationApp(App):
+    def build(self):
+        self.layout = BoxLayout(orientation='vertical')
+        
+        self.lat_label = Label(text="Latitude: Fetching...")
+        self.long_label = Label(text="Longitude: Fetching...")
+        self.button = Button(text="Fetch Location", on_press=self.get_location)
+        
+        self.layout.add_widget(self.lat_label)
+        self.layout.add_widget(self.long_label)
+        self.layout.add_widget(self.button)
+        
+        return self.layout
+
+    def get_location(self, instance):
+        g = geocoder.ip('me')
+        location = g.latlng
+        if location:
+            self.lat_label.text = f"Latitude: {location[0]}"
+            self.long_label.text = f"Longitude: {location[1]}"
+            print("Latitude: ",location[0])
+            print("Longitude: ",location[1])
+        else:
+            self.lat_label.text = "Unable to fetch location"
+            self.long_label.text = ""
+
+if __name__ == "__main__":
+    LocationApp().run()
