764cdf41257eba6bcd27d9c1a52711b0670b1789
Vimal Singh <157964050+its-Vimal@users.noreply.github.com>
2024-09-06 06:31:38 +0530

    Update location.py

--- location.py
+++ location.py
@@ -1,36 +1,13 @@
 import geocoder
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 
-class LocationApp(App):
-    def build(self):
-        self.layout = BoxLayout(orientation='vertical')
-        
-        self.lat_label = Label(text="Latitude: Fetching...")
-        self.long_label = Label(text="Longitude: Fetching...")
-        self.button = Button(text="Fetch Location", on_press=self.get_location)
-        
-        self.layout.add_widget(self.lat_label)
-        self.layout.add_widget(self.long_label)
-        self.layout.add_widget(self.button)
-        
-        return self.layout
+def get_current_location():
+    g = geocoder.ip('me')  # Fetches the location using the IP address
+    return g.latlng
 
-    def get_location(self, instance):
-        g = geocoder.ip('me')
-        location = g.latlng
-        if location:
-            self.lat_label.text = f"Latitude: {location[0]}"
-            self.long_label.text = f"Longitude: {location[1]}"
-            Lat_itude=location[0]
-            Long_itude=location[1]
-            print("Lattitude: ",Lat_itude)
-            print("Longitude: ",Long_itude)
-        else:
-            self.lat_label.text = "Unable to fetch location"
-            self.long_label.text = ""
+# Call the function
+location = get_current_location()
 
-if __name__ == "__main__":
-    LocationApp().run()
+if location:
+    print(f"Current location: Latitude: {location[0]}, Longitude: {location[1]}")
+else:
+    print("Unable to fetch the location.")
